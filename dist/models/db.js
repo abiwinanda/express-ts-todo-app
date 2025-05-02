b0baf6b9a25e95ff04ccbd2b7e35f9e99fb42aee
abiwinanda <abiwinanda@outlook.com>
2025-05-02 20:48:02 +0300

    remove dist

--- dist/models/db.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.todos = exports.users = void 0;
-exports.users = new Map();
-// key = username
-exports.todos = new Map();
